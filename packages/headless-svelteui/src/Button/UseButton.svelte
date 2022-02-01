711ea8a596226a0f47fdb9f29a1963f8c427103c
calebbenjin <calebben94@gmail.com>
2022-02-01 11:12:40 +0100

    Sveltecore-component-library

---
+++ packages/headless-svelteui/src/Button/UseButton.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { styled } from '../theme/src/index'
+  import theme from '../theme/theme'
+
+  function handleClick() {
+    buttonText += " Clicked";
+  }
+</script>
+
+<button on:click={handleClick} use:styled={[$$props, $theme]} ><slot /></button>
